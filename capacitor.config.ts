56481f8ab74c3e9fda04c31daa8170fdfe9ef036
melkzminely.silva <melkzminely.silva@totvs.com.br>
2023-10-16 16:05:19 -0300

    Ionic project init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fitness-concept',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
